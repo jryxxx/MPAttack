f233b2b9f9dbec3368a92e4274a9c5cd9f2c4779
jryxxx <64191196+jryxxx@users.noreply.github.com>
2025-07-23 11:28:40 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,11 +1,3 @@
-#!/usr/bin/python3
-# _*_ coding: utf-8 _*_
-
-# @Date        : 2025/04/15 15:06
-# @Author      : Ruiyang Jia
-# @File        : train.py
-# @Software    : Visual Studio Code
-# @Description :
 
 import warnings
 import os
